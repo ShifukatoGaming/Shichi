fec72c7e13e2dca6b348ecd8abf3e738b4811f4b
Ryan C. Adams <magicalturtle96@gmail.com>
2015-02-08 17:36:31 -0500

    First Commit

---
+++ main/java/io/github/shifukatogaming/shichi/ShichiMain.java
@@ -0,0 +1,28 @@
+package io.github.shifukatogaming.shichi;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ShichiMain.MODID, name = ShichiMain.NAME, version = ShichiMain.VERSION)
+public class ShichiMain 
+{
+	
+	public static final String MODID = "";
+	public static final String NAME = "MafiaCraft";
+	public static final String VERSION = "0.0.1-ALPHA";
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) 
+	{
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) 
+	{
+		
+	}
+	
+}
